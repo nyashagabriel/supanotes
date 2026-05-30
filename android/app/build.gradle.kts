c1809d8b5af75b570e7e568eeede98ef7aa0f0d7
Gabriel Kuudzadombo <gabwixgamesite2024@gmail.com>
2026-05-30 18:35:15 +0200

    xyfs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,32 +20,33 @@ android {
     }
 
     signingConfigs {
-        release {
-            storeFile file('release-key.jks')
-            storePassword System.getenv("KEYSTORE_PASSWORD")
-            keyAlias System.getenv("KEY_ALIAS")
-            keyPassword System.getenv("KEY_PASSWORD")
+        create("release") {
+            storeFile = file("release-key.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
         }
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.greywayco.supanotes"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        resConfigs "en" // Add only your languages here
+        
+        resourceConfigurations.add("en")
     }
 
-buildTypes {
-        release {
-            signingConfig signingConfigs.debug // Change to your release config
-            minifyEnabled true       // MANDATORY: Enables R8 code shrinking
-            shrinkResources true      // MANDATORY: Enables unused asset removal
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), 
+                "proguard-rules.pro"
+            )
         }
     }
 }
